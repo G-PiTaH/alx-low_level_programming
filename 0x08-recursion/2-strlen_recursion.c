cd7ad18bed6a10091c6451244ed51b1949dfaa56
Peter Momanyi <itspeter254@gmail.com>
2023-08-30 04:39:12 -0700

    C Recursion task 0

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+/********** C RECURSION **********/
+/* AUTHOR: PETER MOMANYI */
+/* DATE: 30TH AUGUST 2023 */
+
+#include "main.h"
+
+/**
+ * _strlen_recursion - length of a string recursion
+ * @s: string char
+ *
+ * Return: Number of characters
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	return 1 + _strlen_recursion(s + 1);
+}
