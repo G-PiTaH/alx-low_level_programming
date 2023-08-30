671d377ef71e7e5aabd5e84111f23550977b0c16
Peter Momanyi <itspeter254@gmail.com>
2023-08-30 04:00:31 -0700

    C Recursion task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,24 +1,24 @@
 /********** C RECURSION **********/
-/* AUTHOR: PETER MOMANYI
- *  DATE: 30TH AUGUST 2023
- */
+/* AUTHOR: PETER MOMANYI */
+/* DATE: 30TH AUGUST 2023 */
 
 #include "main.h"
+
 /*
- * _puts_recursion: Prints string followed with a new line
- *
+ * _puts_recursion - works like puts function
  * @s: string character
  *
- * Return: Ther string 
+ * Return: 0 for success (Always)
  */
 void _puts_recursion(char *s)
 {
-	if ( *s == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 
 		return;
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	else
+		_putchar(*s);
+		_puts_recursion(s + 1);
 }
