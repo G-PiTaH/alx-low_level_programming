aac44cbcd8416d4ef8217c5e5b5b2c8895243575
Peter Momanyi <itspeter254@gmail.com>
2023-08-30 04:47:48 -0700

    C Recursion task 0

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+/********** C RECURSION **********/
+/* AUTHOR: PETER MOMANYI */
+/* DATE: 30TH AUGUST 2023 */
+#include "main.h"
+
+/**
+ * factorial - factorial of a given number
+ * @n: integer to find factorial
+ *
+ * Return: The factorial
+ */
+int factorial(int n)
+{
+	if (n == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return(n*factorial(n-1);
+	}
+}
